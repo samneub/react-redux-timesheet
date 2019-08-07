c50706a972c714019d2e9133e9e8ce7c191ce8f2
Sam Neubauer <srneubauer@spscommerce.com>
2019-08-07 11:33:53 -0500

    Lab2

--- lab-02/src/App.js
+++ lab-02/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import Projects from "./projects/Projects";
+import Projects from './projects/Projects';
+import ProjectRow from './projects/ProjectRow';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div className="container">
+          <Projects />
+        </div>
       </div>
     );
   }
 }
+
+// {/* <ProjectRow project={name: "cat", description: "magic}" /> */}
 
 export default App;
--- lab-02/src/projects/ProjectRow.test.js
+++ lab-02/src/projects/ProjectRow.test.js
@@ -4,5 +4,32 @@ import { shallow } from 'enzyme';
 import ProjectRow from './ProjectRow';
 
 describe('<ProjectRow />', () => {
-  // TODO Add tests
+  let wrapper;
+  beforeEach(() => {
+    const project = {
+      name: 'NAME',
+      description: 'DESCRIPTION'
+    };
+    wrapper = shallow(<ProjectRow project={project} />);
+  });
+
+  it('should instantiate the Prject Row Componeent', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('should instantiate the Prject Row Componeent', () => {
+    expect(wrapper.find('td')).toHaveLength(2);
+    expect(
+      wrapper
+        .find('td')
+        .at(0)
+        .text()
+    ).toEqual('NAME');
+    expect(
+      wrapper
+        .find('td')
+        .at(1)
+        .text()
+    ).toEqual('DESCRIPTION');
+  });
 });
